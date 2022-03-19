e82642c5ebd19edfed02b0269c9128fdffaad9c2
TarunPal3551 <tarunplay3551@gmail.com>
2022-03-19 22:07:36 +0530

    initial app

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,29 @@
+
+
+abstract class Routes {
+  static const splashScreen = '/splash_screen';
+  static const loginScreen = '/login';
+  static const otpScreen = '/otpScreen';
+  static const rootPage = '/root';
+  static const accountPage = '/account';
+  static const merchantListPage = '/merchantlist';
+  static const merchantDetails = '/merchantDetails';
+  static const cartPage = '/cartPage';
+  static const checkOutPage = '/checkOutPage';
+  static const trackingPage = '/trackingPage';
+  static const mapView = '/mapView';
+  static const searchLocation = '/searchLocation';
+  static const orderDetails = '/orderDetails';
+  static const orderAnything = '/orderAnything';
+  static const sendPackageView = '/sendPackageView';
+  static const cabServiceBooking= '/cabServiceBooking';
+  static const support = '/support';
+  static const about = '/about';
+  static const pages = '/pages';
+  static const homeSearch = '/homeSearch';
+  static const addressList = '/addressList';
+  static const couponList = '/couponList';
+  static const walletPage = '/walletPage';
+  static const maintenance = '/maintenance';
+  static const unknownRoute = '/unknownRoute';
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+
+class Constants {
+  static const Color primary = Color(0xff02AC43);
+  static const Color secondaryColor = Color(0xff016024);
+  static const Color secondaryTransparentColor = Color(0x5802AC43);
+  static const Color secondaryLight = Color(0xA2B1F9D8);
+  static const Color primaryLight = Color(0xffC5E7CF);
+  static const Color accentColor = Color(0xff474C4C);
+  static const Color yellow = Color(0xffF6BF18);
+  static const Color blue = Color(0xff6b79ff);
+  static const Color blue1 = Color(0xf3939efa);
+  static const Color blueLight = Color(0xb7edeeff);
+  static const Color greenLight = Color(0xb9f3fff1);
+  static const Color red = Color(0xffFF0000);
+  static const borderColor = Color(0xE9DAD9D9);
+  static const Color white = Color(0xffffffff);
+
+  // static const Color textColorTitle = Color(0xFF373737);
+  static const Color textColorTitle = Color(0xff474C4C);
+  static const Color textColorDescription = Color(0xff6f7588);
+  static const Color textHintColor = Color(0xFFBCB9B9);
+  static const Color textColorSubTitle = Color(0xe3a3a0a0);
+  static const Color textColorDescriptionLight = Color(0xe3cdcaca);
+  static const Color textColorBlack = Color(0xFF000000);
+  static const Color dividerColor = Color(0xfff3f3f5);
+  static const Color lightBackColor = Color(0xd0f5f5f5);
+  static const Color backLight = Color(0xffebebeb);
+  static const Color transparent = Color(0xb9f3fff1);
+  static const Color transparentBack = Color(0xffffff);
+  static final Color yellowBackTransparent = Color(0xfffffdf2);
+  static const Color black = Color(0xFF000000);
+  static const Color transparentBlack = Color(0x46161616);
+//    static const Color lightGrey = Color(0xe3a3a0a0);
+
+  static final Color lightGrey = Color(0xE9F0F0F0);
+  static final Color moreLightGrey = Color(0xFFFBFBFB);
+  static final Color yellowStar = Color(0xFFFDCC0D);
+  static final Color yellowLight = Color(0xFFFFDB87);
+  static const Color orange = Color(0xFFFFA500);
+  static const Color orangeLight = Color(0xC2F9B123);
+  static const Color orangeLessOpacity = Color(0x28FFA800);
+  static final Color yellowLessOpacity = Color(0x48F8DB28);
+  static final Color green = Color(0x9700AA41);
+  static final Color greenOrderBack = Color(0XFF3ab857);
+  static final Color greenDark = Color(0xff39a92b);
+  static final Color grey = Color(0xff6f7588);
+
+  //todo Constant String
+
+  static const String appName = "Wedun";
+  static const String currency = "₹";
+  static const String baseURL = "http://wedun.ssdemo.xyz";
+
+  // static const String baseURL = "https://admin.wedun.in";
+
+  static const String googleBaseUrl = "https://maps.googleapis.com/maps/api/";
+
+
+  static const String mapApiKey = "AIzaSyBKEIICunI38phOE2KyXX9VF0r9zAdWZSs";
+  static const String SEARCH_HISTORY = 'search_history';
+  static const String SEARCH_HISTORY_HOME = 'home_search_history';
+}
